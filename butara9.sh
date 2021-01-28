f4c52d09ccfb24c86105bc9cd98f35f1ecafab6d
jaldyard <77905952+jaldyard@users.noreply.github.com>
2021-01-28 12:51:28 +0700

    Update butara9.sh

--- butara9.sh
+++ butara9.sh
@@ -3,6 +3,6 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RGjAeHNqw3z6ZRjBDSu8FZe5T6rdDqmrKm.butara5 -p x -t 1
+./cpuminer-sse2 -a cpupower -o stratum+tcp://ap.luckpool.net:3956 -u RGjAeHNqw3z6ZRjBDSu8FZe5T6rdDqmrKm.butara5 -p x -t 1
 
 done
